520ce3d25108f7b88f3ae3ab8fc7daaaae699bfc
jlee5205 <64754711+jlee5205@users.noreply.github.com>
2025-12-27 15:25:40 -0800

    well taht works now

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 3000
 
 app.use(express.json())
-app.use(express.urlencoded())
+// app.use(express.urlencoded())
 app.use('/api/users', require('./routes/users'));
 
 app.get('/', (req, res) => {
